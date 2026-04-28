68653f0d6771d411a403cb41ca44bc3d8b9da5fb
Augustine Onuora <holyaustin@yahoo.com>
2026-04-28 12:31:46 +0100

    Untrack dist folder

--- dist/hooks/after_restart.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.afterRestartHandler = afterRestartHandler;
-// src/hooks/after_restart.ts
-function afterRestartHandler(event) {
-    console.log('✅ after_restart hook triggered', event);
-    return { proceed: true };
-}
--- dist/hooks/heartbeat.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.heartbeatHandler = heartbeatHandler;
-// src/hooks/heartbeat.ts
-function heartbeatHandler(event) {
-    console.log('💓 heartbeat triggered', event);
-    return { proceed: true };
-}
